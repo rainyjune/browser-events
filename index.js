47969f341e991bba20dbbbda59628f484c2a12bc
rainyjune <rainyjune@live.cn>
2019-05-24 12:14:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 window.onload = function() {
 
-  var div1 = document.getElementById('div1'),
-      div2 = document.getElementById('div2'),
-      testp = document.getElementById('testp'),
-      testa = document.getElementById('testlink');
+  var div1 = id('div1'),
+      div2 = id('div2'),
+      testp = id('testp'),
+      testa = id('testlink');
+
+  var captureToggleBtn = id('captureToggleBtn'),
+      bubbleToggleBtn = id('bubbleToggleBtn'),
+      captureToggleStatus = id('captureToggleStatus'),
+      bubbleToggleStatus = id('bubbleToggleStatus'),
+      logarea = id('logarea'),
+      clearlog = id('clearlog');
 
   var arr = [div1, div2, testp];
 
   var captureListener = function(e) {
-    alert('capturing phase: #' + this.id);
+    console.log('capturing phase: #' + this.id);
+    writelog('capturing phase: #' + this.id);
   };
 
   var bubbleListener = function(e) {
-    alert('bubbling phase: #' + this.id);
+    console.log('bubbling phase: #' + this.id);
+    writelog('bubbling phase: #' + this.id);
   };
 
   for (var i = 0, len = arr.length; i < len; i++) {
@@ -22,19 +31,66 @@ window.onload = function() {
     addEvent(thisElement, 'click', bubbleListener, false);
   }
 
-  addEvent(div2, 'click', function(e) {
-    //debugger;
-    var evt = fixEvent(e);
-    //window.event.cancelBubble = true;
-    evt.stopPropagation();
-    alert('stopPropagation on #div2');
-  }, false);
+  id('isCaptureSupported').innerText = document.addEventListener ? '是' : '否';
+  if (document.addEventListener) {
+    captureToggleBtn.disabled = false;
+    captureToggleBtn.innerText = '为捕获阶段删除监听器';
+  }
 
-  addEvent(testa, 'click', function(e) {
+  function id(str) {
+    return document.getElementById(str);
+  }
+
+  addEvent(captureToggleBtn, 'click', function(e) {
+    writelog('--------');
+
+    var status = this.getAttribute('data-status');
     e = fixEvent(e);
-    alert('testa clicked');
-    e.preventDefault();
+    if (status === 'on') {
+      this.setAttribute('data-status', 'off');
+      captureToggleBtn.innerText = '为捕获阶段增加监听器';
+      log(captureToggleStatus, '捕获阶段的监听器已经注销');
+    } else {
+      this.setAttribute('data-status', 'on');
+      captureToggleBtn.innerText = '为捕获阶段删除监听器';
+      log(captureToggleStatus, '捕获阶段的监听器已经注册');
+    }
+    for (var i = 0, len = arr.length; i < len; i++) {
+      var thisElement = arr[i];
+      if (status === 'on') {
+        removeEvent(thisElement, 'click', captureListener, true);
+      } else {
+        addEvent(thisElement, 'click', captureListener, true);
+      }
+    }
   });
+
+  addEvent(bubbleToggleBtn, 'click', function(e) {
+    writelog('--------');
+    var status = this.getAttribute('data-status');
+    e = fixEvent(e);
+    if (status === 'on') {
+      this.setAttribute('data-status', 'off');
+      bubbleToggleBtn.innerText = '为冒泡阶段增加监听器';
+      log(bubbleToggleStatus, '冒泡阶段的监听器已经注销');
+    } else {
+      this.setAttribute('data-status', 'on');
+      bubbleToggleBtn.innerText = '为冒泡阶段删除监听器';
+      log(bubbleToggleStatus, '冒泡阶段的监听器已经注册');
+    }
+    for (var i = 0, len = arr.length; i < len; i++) {
+      var thisElement = arr[i];
+      if (status === 'on') {
+        removeEvent(thisElement, 'click', bubbleListener, false);
+      } else {
+        addEvent(thisElement, 'click', bubbleListener, false);
+      }
+    }
+  });
+
+  clearlog.onclick = function() {
+    logarea.value = '';
+  };
 
   function addEvent(target, type, listener, useCapture) {
     if (document.addEventListener) {
@@ -46,7 +102,31 @@ window.onload = function() {
         return listener.apply(target, arguments);
       };
       target.attachEvent('on' + type, bound);
+      target['my' + type] = bound;
       return bound;
+    }
+  }
+
+  function removeEvent(target, type, listener, useCapture) {
+    if (document.removeEventListener) {
+      target.removeEventListener(type, listener, useCapture);
+    } else if (document.detachEvent) {
+      if (useCapture) return ;
+      target.detachEvent('on' + type, listener);
+      target['my' + type] = null;
+    }
+  }
+
+  function log(dom, text) {
+    dom.innerText = text;
+  }
+
+  function writelog(text) {
+    logarea.value += "\n" + text;
+    if (logarea.scrollTo) {
+      logarea.scrollTo({
+        top: logarea.scrollHeight
+      });
     }
   }
 
